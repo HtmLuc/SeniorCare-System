3ccb64aa72cfa4e8f2ce06e54dd1426a3fc20b3f
Lucas Dantas <lucasmedeiros@trt21.jus.br>
2026-01-30 08:22:14 -0300

    feat: add POST endpoint for user creation

--- src/main/java/com/htmluc/SeniorCare_System/controller/UserController.java
+++ src/main/java/com/htmluc/SeniorCare_System/controller/UserController.java
@@ -1,5 +1,37 @@
 package com.htmluc.SeniorCare_System.controller;
 
+import com.htmluc.SeniorCare_System.model.UserModel;
+import com.htmluc.SeniorCare_System.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/user")
 public class UserController
 {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping
+    public ResponseEntity create(@RequestBody UserModel userModel)
+    {
+        try
+        {
+            var user = this.userRepository.findById(userModel.getId());
+            if(userModel.getId() != null && this.userRepository.existsById(userModel.getId()))
+            {
+                throw new IllegalArgumentException("User already exists");
+            }
+            this.userRepository.save(userModel);
+            return ResponseEntity.ok().build();
+        }
+        catch (Exception e)
+        {
+            return ResponseEntity.badRequest().build();
+        }
+    }
 }
