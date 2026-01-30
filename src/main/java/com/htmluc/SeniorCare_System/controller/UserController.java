0cb6af00aaa6f9427e6aed1d1d3f8ee658c9b1e0
Lucas Dantas <lucasmedeiros@trt21.jus.br>
2026-01-30 09:19:40 -0300

    docs: add user documentation

--- src/main/java/com/htmluc/SeniorCare_System/controller/UserController.java
+++ src/main/java/com/htmluc/SeniorCare_System/controller/UserController.java
@@ -2,6 +2,8 @@ package com.htmluc.SeniorCare_System.controller;
 
 import com.htmluc.SeniorCare_System.model.UserModel;
 import com.htmluc.SeniorCare_System.repository.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,12 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/user")
+@Tag(name = "User", description = "User Controller for managing endpoints for creation, retrieval, and updates user data.")
 public class UserController
 {
     @Autowired
     private UserRepository userRepository;
 
     @PostMapping
+    @Operation(summary = "Save user data", description = "Method for saving user data.", deprecated = false)
+    @ApiResponse(responseCode = "201", description = "User created successfully")
+    @ApiResponse(responseCode = "400", description = "user already exists")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
     public ResponseEntity create(@RequestBody @Valid UserModel userModel)
     {
         try
