6e28036668968d899b18dd4a219129af57d8b95d
Lucas Dantas <lucasvdmedeiros@proton.me>
2026-02-02 15:45:14 -0300

    fix: correct routes

--- src/main/java/com/htmluc/SeniorCare_System/repository/FamilyContactRepository.java
+++ src/main/java/com/htmluc/SeniorCare_System/repository/FamilyContactRepository.java
@@ -4,8 +4,8 @@ import com.htmluc.SeniorCare_System.model.FamilyContactModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
-import java.util.UUID;
 
-public interface FamilyContactRepository extends JpaRepository<FamilyContactModel, Long> {
-   Optional<FamilyContactModel> findByPatient_Id(UUID patientId);
+public interface FamilyContactRepository extends JpaRepository<FamilyContactModel, Long>
+{
+    Optional<FamilyContactModel> findByPersonId(Long id);
 }
