ecfe036761238758a47395d2a2eac8f61a259cb5
Lucas Dantas <lucasvdmedeiros@proton.me>
2026-02-03 10:04:05 -0300

    feat: add PUT endpoint for monitoring list

--- src/main/java/com/htmluc/SeniorCare_System/controller/MonitoringController.java
+++ src/main/java/com/htmluc/SeniorCare_System/controller/MonitoringController.java
@@ -2,9 +2,11 @@ package com.htmluc.SeniorCare_System.controller;
 
 import com.htmluc.SeniorCare_System.model.MonitoringModel;
 import com.htmluc.SeniorCare_System.repository.MonitoringRepository;
+import com.htmluc.SeniorCare_System.repository.PatientRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -43,5 +45,28 @@ public class MonitoringController
     public ResponseEntity<MonitoringModel> getById(@PathVariable Long id)
     {
         return this.monitoringRepository.findById(id).map(monitoring -> ResponseEntity.ok(monitoring)).orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    @Operation(summary = "Update monitoring by ID", description = "Updates an existing monitoring with the provided data")
+    @ApiResponse(responseCode = "200", description = "Monitoring updated successfully")
+    @ApiResponse(responseCode = "404", description = "Monitoring not found")
+    @ApiResponse(responseCode = "400", description = "Invalid input data")
+    @ApiResponse(responseCode = "500", description = "Internal server error")
+    public ResponseEntity<MonitoringModel> updateById(@PathVariable Long id, @RequestBody MonitoringModel monitoringModel)
+    {
+        return this.monitoringRepository.findById(id).map(info -> {
+            info.setName(monitoringModel.getName());
+            info.setBloodSugar(monitoringModel.getBloodSugar());
+            info.setBloodPressure(monitoringModel.getBloodPressure());
+            info.setHeartRate(monitoringModel.getHeartRate());
+            info.setRespiratoryRate(monitoringModel.getRespiratoryRate());
+            info.setSaturation(monitoringModel.getSaturation());
+            info.setTemperature(monitoringModel.getTemperature());
+            info.setTimeMeasure(monitoringModel.getTimeMeasure());
+
+            MonitoringModel monitoring = this.monitoringRepository.save(info);
+            return ResponseEntity.ok(monitoring);
+        }).orElse(ResponseEntity.notFound().build());
     }
 }
